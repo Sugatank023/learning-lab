18a1c8381fda2b41280e6f6601929bff1d1489bf
Sugatank023 <sugatank0513@gmail.com>
2026-03-01 22:05:25 +0900

    連想配列　練習問題

---
+++ php/ai/Gemini/3-1-2/a-array3.php
@@ -0,0 +1,18 @@
+<?php
+//連想配列 練習問題
+
+$scores=[
+    "math"=>85,
+    "english"=>70,
+    "japanese"=>90
+];
+
+$name=["数学","英語","国語"];
+$eng_name=["math","english","japanese"];
+
+$nameCount=count($name);
+for($i=0;$i<$nameCount;$i++){
+    echo $name[$i]."は".$scores[$eng_name[$i]]."点です。<br>";
+}
+
+?>
