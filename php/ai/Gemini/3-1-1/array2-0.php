7020814e39e72042550281516213f95262c334c3
Sugatank023 <sugatank0513@gmail.com>
2026-03-01 20:54:45 +0900

    配列練習問題

---
+++ php/ai/Gemini/3-1-1/array2-0.php
@@ -0,0 +1,12 @@
+<?php
+
+//問題回答
+$ftuits=["りんご","みかん"];
+$ftuits[]="ブドウ";
+$ftuits[0]="いちご";
+
+echo"<pre>";
+print_r($ftuits);
+echo"<pre>";
+
+?>
---
+++ php/ai/Gemini/3-1-1/array3.php
@@ -0,0 +1,9 @@
+<?php
+
+//練習問題
+$cities=["東京","大阪","名古屋","福岡"];
+$length=count($cities);
+
+echo"第3の都市は{$cities[2]}です。";
+
+?>
