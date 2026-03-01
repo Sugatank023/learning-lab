6a08622bc9ca6ddca9164b74a0d5eca76afdefd9
Sugatank023 <sugatank0513@gmail.com>
2026-03-01 22:18:29 +0900

    連想配列　応用問題

--- php/ai/Gemini/3-1-2/a-array4.php
+++ php/ai/Gemini/3-1-2/a-array4.php
@@ -1,1 +1,16 @@
+<?php
+//連想配列　応用問題
 
+$item=[
+    "name"=>"PC",
+    "price"=>100000,
+    "quantity"=>2
+];
+
+$total_price=0;
+$total_price=$item["price"]*$item["quantity"];
+
+echo "計算式は".$item["price"]."[円]×".$item["quantity"]."[台]={$total_price}[円]<br>";
+echo "PCの合計金額は{$total_price}円です。";
+
+?>
