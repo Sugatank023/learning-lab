8aae53d5f1da533695cd852ab4d35682a662ba3c
Sugatank023 <sugatank0513@gmail.com>
2026-03-02 16:48:22 +0900

    関数（引数、戻り値、スコープ）　応用問題

--- php/ai/Gemini/3-2-3/scope5.php
+++ php/ai/Gemini/3-2-3/scope5.php
@@ -2,27 +2,27 @@
 //関数　引数、戻り値、スコープ
 
 //追加応用問題
-function calculateShipping($weight){
-    if($weight>=10){
-        return(1500);
-    }elseif($weight<5){
-        return (500);
-    }else{
+function calculateBuffetPrice($age){
+    if($age>=65){
+        return (2000);
+    }elseif($age>=13){
+        return (3000);
+    }elseif($age>=3){
         return (1000);
+    }else{
+        return (0);
     }
 }
 
-
-//呼び出し
-$weights=[3,8,12];
+$family_ages=[35,32,8,2];
 $total_price=0;
 
-foreach($weights as $weight){
-    $price=calculateShipping($weight);
-    echo "重量{$weight}kgの送料は{$price}円になります。<br>";
-    $total_price+=$price;
+foreach($family_ages as $age ){
+    $one_price=calculateBuffetPrice($age);
+    echo"{$age}歳のお客さまの料金は{$one_price}円です。<br>";
+    $total_price+=$one_price;
 }
 
-echo"合計は{$total_price}円です。";
+echo"<hr>家族全員の料金は{$total_price}円です。";
 
 ?>
