ff3bcff160e7fcfc325caa4d30cf213a9d08506d
Sugatank023 <sugatank0513@gmail.com>
2026-03-02 18:50:35 +0900

    フォームデータの送受信（GET）

---
+++ php/ai/Gemini/4-1-1/practice_form1.php
@@ -0,0 +1,21 @@
+
+<form method="GET" action="">
+    <label>お名前：</label>
+    <input type="text" name="user_name">
+    <buttton type="submit">送信</button>
+</form>
+
+<hr>
+
+<?php
+//PHP部分：送られてきたデータを受け取る処理
+//$_GETは、GETで送られてきたデータが入る「特別な連想配列」
+
+//もし'user_name'というデータが送られてきたら、（isset関数で確認）
+
+if(isset($_GET['user_name'])){
+    $name=$_GET['user_name'];
+    echo "こんにちは、{$name}さん！（GETで受け取りました）";
+}
+
+?>
---
+++ php/ai/Gemini/4-1-1/practice_form2.php
@@ -0,0 +1,15 @@
+
+<form method="POST" action="">
+    <label>お名前；</form>
+    <input type="text" name="user_name">
+    <button type="submit">送信</button>
+</form>
+
+
+
+<?php
+// PHP部分：送られてきたデータを受け取る処理
+// $_POST は、POSTで送られてきたデータが入る「特別な連想配列」です
+
+//もし'user_name'というデータが送られてきたら（isset関数で確認）
+?>
