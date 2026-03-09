6a01a662a6558735e932feada089f8b2ef8bc77f
Sugatank023 <sugatank0513@gmail.com>
2026-03-09 21:30:18 +0900

    セキュリティ　例題

---
+++ php/ai/Gemini/4-1-3/security4.php
@@ -0,0 +1,18 @@
+<form method="POST" action="">
+    <label>お名前：</label>
+    <input type="text" name="user_name">
+    <button type="submit">送信</button>
+</form>
+
+<hr>
+
+<?php
+//issetで確認
+if(isset($_POST["user_name"])){
+    //変数代入（サニタイズ）
+    $user_name=htmlspecialchars($_POST["user_name"]);
+    //表示処理
+    echo"こんにちは、{$user_name}さん";
+}
+
+?>
