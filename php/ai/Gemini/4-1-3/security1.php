655ba25ff132bddcfba4716217f9fe8c6df2eab4
Sugatank023 <sugatank0513@gmail.com>
2026-03-09 20:52:01 +0900

    セキュリティ　例

--- php/ai/Gemini/4-1-3/security1.php
+++ php/ai/Gemini/4-1-3/security1.php
@@ -1,6 +1,12 @@
 <?php
+//例題
 if(isset($_POST["user_name"])){
+    //危険な書き方（今まで）
+    //$name=$_POST["user_name"];
 
+    //安全な書き方（サニタイズ）
+    $name=htmlspecialchars($_POST["user_name"],ENT_QUOTES,'UTF-8');
+    echo"こんにちは{$name}さん";
 }
 
 ?>
