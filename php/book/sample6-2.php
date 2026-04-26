6b377d4382906eae1f9037dd20b3132377e032ff
Sugatank023 <sugatank0513@gmail.com>
2026-04-26 23:25:08 +0900

    Update sample6-2.php

--- php/book/sample6-2.php
+++ php/book/sample6-2.php
@@ -1,18 +1,26 @@
 <?php
+//クラスの学習ファイル
 
+//クラスファイルの読み込み
 require_once("car.php");
 
+//インスタンスの生成
 $car1 = new Car();
 $car2 = new Car();
 
+//プロパティ
 $car1->number = "あ12-34";
 $car1->speed = 50;
+
+//メゾット
 $car1->drive();
 $car1->stop();
 
+//繰り返し
 $car2->number = "い56-67";
 $car2->speed = 70;
 $car2->drive();
 $car2->stop();
 
-?>
+
+?>                  
