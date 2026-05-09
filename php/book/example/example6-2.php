d034a2e28743948733cf8d3182a46830527d524f
Sugatank023 <sugatank0513@gmail.com>
2026-05-09 20:59:36 +0900

    クラス　例題6-2

---
+++ php/book/example/example6-2.php
@@ -0,0 +1,15 @@
+<?php
+/*
+class City{
+    public $name;
+    public $prefecture;
+}
+
+$c = new City();
+
+$c->name = "横浜市";
+$c->prefecture = "神奈川県";
+
+echo "{$c->name}は{$c->prefecture}にあります。<br>";
+*/
+?>
---
+++ php/book/example/example6-2ans.php
@@ -0,0 +1,32 @@
+<?php
+
+class City{
+    private $name;
+    private $prefecture;
+
+    function setName($name){
+        $this->name=$name;
+    }
+    function getName(){
+        return $this->name;
+    }
+    function setPrefecture($prefecture){
+        $this->prefecture = $prefecture;
+    }
+    function getPrefecture(){
+        return $this->prefecture;
+    }
+}
+
+$c = new City();
+$c->setName("横浜市");
+$c->setprefecture("神奈川県");
+echo "{$c->getName()}は{$c->getPrefecture()}にあります。<br>";
+
+$c2 = new City();
+$c2->setName("大阪市");
+$c2->setprefecture("大阪府");
+echo "{$c2->getName()}は{$c2->getPrefecture()}にあります。<br>";
+
+
+?>
