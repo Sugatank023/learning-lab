20892834f74aa3104773bd91a41ab55cf02e3c40
Sugatank023 <sugatank0513@gmail.com>
2026-08-16 21:12:48 +0900

    条件付きSELECT文

--- php/book/sample2.sql
+++ php/book/sample2.sql
@@ -1,6 +1,6 @@
 
-#データベースとカラムの表示
-#SELECT * FROM データベース名.テーブル名;
+#データベースとカラムの表示 SELECT文
+SELECT * FROM データベース名.テーブル名;
 SELECT * FROM school.student;
 #実行結果
 id	name	grade	
@@ -11,9 +11,8 @@ id	name	grade
 1005	林敦子	3	
 1006	市村次郎	3	
 
-
-#データベースとカラムの表示
-#SELECT カラム名1, カラム名2 FROM データベース名.テーブル名;
+#指定カラムの表示　SELECT文
+SELECT カラム名1, カラム名2,... FROM データベース名.テーブル名;
 SELECT id, name FROM school.student;
 #実行結果
 id	name	
@@ -23,3 +22,13 @@ id	name
 1004	佐藤元	
 1005	林敦子	
 1006	市村次郎	
+
+#条件付きSELECT文
+SELECT ... FROM データベース名.テーブル名 WHERE 条件式;
+SELECT * FROM school.student WHERE grade = 2;
+#実行結果
+id	name	grade	
+1003	太田隆	2	
+1004	佐藤元	2	
+果
+
